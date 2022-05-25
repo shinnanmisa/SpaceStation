9d8595894d40863be0b5ba5fb6deaa593c5f7d65
Xyl <31138624+shinnanmisa@users.noreply.github.com>
2022-05-25 17:57:33 +0800

    Delete asm.js

--- js/asm.js
+++
@@ -1,7 +0,0 @@
-// When the user clicks on <div>, open the popup
-/*<script>
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-}
-</script>*/
